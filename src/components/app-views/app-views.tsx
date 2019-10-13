1e6eb832a03f65e2dc85094fd71e46e1912ce82b
Andrei Vasilache <vasilacheandrei46@gmail.com>
2019-10-13 07:17:38 -0700

    default redirect to today

--- src/components/app-views/app-views.tsx
+++ src/components/app-views/app-views.tsx
@@ -1,7 +1,7 @@
 import history from '../../helpers/history';
 
 import React , { Component } from 'react';
-import { Route, Router } from "react-router-dom";
+import { Route, Router, Redirect } from "react-router-dom";
 
 import TodayComponent from './today/today';
 import FutureComponent from './future/future';
@@ -9,9 +9,9 @@ import FutureComponent from './future/future';
 
 class AppViews extends Component{
     render(){
-        {console.log(history)};
         return(
             <Router history={history}>
+                    <Redirect exact from="/" to="today" />
                     <Route exact path={`/today`} component={TodayComponent}></Route>
                     <Route  path={`/future`} component={FutureComponent}></Route>
             </Router>
